99b3ff11383a02d9c3beee85153ca644aa78c626
jasmine-sail <138678437+jasmine-sail@users.noreply.github.com>
2023-07-05 21:00:08 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Hello world");
+
+var items=["りんご","ゴリラ","ラッパ"];
+console.log(items)
+
+for(var i=0; i<items.length; i++){
+  console.log(items[i])
+}
+
+let sum=0;
+for(let i=1; i<=100;i++){
+  sum+=i;
+}
+console.log(sum);
